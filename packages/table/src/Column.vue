d8322e99eb642f6eca235c35b4ba6a3a87b74e9e
zhangyz <1065049460@qq.com>
2022-02-21 16:58:36 +0800

    表格组件修改

--- packages/table/src/Column.vue
+++ packages/table/src/Column.vue
@@ -1,27 +1,21 @@
 <template>
   <div>
     <template v-for="(column, index) in columns">
-      <el-table-column
-        :key="column.prop"
-        v-if="column.type === 'selection' && show(column)"
-        v-bind="column"
-      ></el-table-column>
 
-      <el-table-column
-        v-else-if="column.type === 'expand' && show(column)"
-        type="expand"
-        :key="column.prop"
-        v-bind="column"
-      >
+      <el-table-column :key="column.prop" v-if="(column.type==='selection' || column.type==='index') && show(column) " v-bind="column"></el-table-column>
+
+      <el-table-column v-else-if="column.type === 'expand' && show(column)" type="expand" :key="column.prop" v-bind="column" >
         <template slot-scope="props">
           <slot name="expand" v-bind="props"></slot>
         </template>
       </el-table-column>
 
       <column-slot v-else-if="show(column)" :column="column" :key="index">
+
         <template :slot="column.prop" slot-scope="props">
           <slot :name="column.prop" v-bind="props"></slot>
         </template>
+
       </column-slot>
     </template>
   </div>
@@ -42,5 +36,3 @@ export default {
   },
 };
 </script>
-
-<style></style>
